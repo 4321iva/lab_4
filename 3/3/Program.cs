52d1068339b68e46dacd17cd773d8cb96c977de5
4321iva <162304549+4321iva@users.noreply.github.com>
2024-10-08 09:16:45 +0300

    commit

---
+++ 3/3/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Приклад масиву цілих чисел
+        int[] numbers = { 5, 8, 13, 22, 35, 40, 55, 60 };
+
+        Console.WriteLine("Парні числа у масиві:");
+
+        // Перевіряємо кожен елемент масиву
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
