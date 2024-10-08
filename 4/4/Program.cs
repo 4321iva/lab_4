11ae0f6dcbd0be8132f84150f02b3458b5704343
4321iva <162304549+4321iva@users.noreply.github.com>
2024-10-08 09:19:17 +0300

    commit

---
+++ 4/4/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Приклад масиву цілих чисел
+        int[] numbers = { -5, 10, -3, 8, 15, -2, 0 };
+
+        // Викликаємо функцію для обчислення суми додатних елементів
+        int sumOfPositive = SumOfPositiveElements(numbers);
+
+        Console.WriteLine($"Сума всіх додатних елементів: {sumOfPositive}");
+    }
+
+    // Функція для знаходження суми додатних елементів масиву
+    static int SumOfPositiveElements(int[] array)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                sum += array[i];
+            }
+        }
+
+        return sum;
+    }
+}
