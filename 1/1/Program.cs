a25b6917c103fb2de3e71bb3a8c8b0cd743153da
4321iva <162304549+4321iva@users.noreply.github.com>
2024-10-08 09:11:31 +0300

    commit

---
+++ 1/1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Приклад масиву цілих чисел
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        int sum = 0;
+
+        // Обчислюємо суму елементів масиву
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        // Обчислюємо середнє арифметичне
+        double average = (double)sum / numbers.Length;
+
+        Console.WriteLine($"Середнє арифметичне масиву: {average}");
+    }
+}
