381fd82adb9d9498bb5798d3aa955a3518307377
4321iva <162304549+4321iva@users.noreply.github.com>
2024-10-08 09:14:16 +0300

    commit

---
+++ 2/2/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Приклад масиву цілих чисел
+        int[] numbers = { 1, 2, 3, 2, 1 };
+
+        bool isPalindrome = true;
+
+        // Перевіряємо, чи є масив паліндромом
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            if (numbers[i] != numbers[numbers.Length - 1 - i])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome)
+        {
+            Console.WriteLine("Масив є паліндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Масив не є паліндромом.");
+        }
+    }
+}
