1e67ab77599dbb87dbc617d4071a38033c0ca96e
4321iva <162304549+4321iva@users.noreply.github.com>
2024-10-11 08:34:09 +0300

    commit

---
+++ 6/6/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Приклад масиву цілих чисел
+        int[] numbers = { -5, 10, -3, 8, -2, 0, 15 };
+
+        // Викликаємо функцію для обчислення добутку від'ємних елементів
+        int productOfNegatives = ProductOfNegativeElements(numbers);
+
+        if (productOfNegatives != 1)
+        {
+            Console.WriteLine($"Добуток всіх від'ємних елементів: {productOfNegatives}");
+        }
+        else
+        {
+            Console.WriteLine("В масиві немає від'ємних елементів.");
+        }
+    }
+
+    // Функція для знаходження добутку всіх від'ємних елементів масиву
+    static int ProductOfNegativeElements(int[] array)
+    {
+        int product = 1;
+        bool hasNegative = false;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < 0)
+            {
+                product *= array[i];
+                hasNegative = true;
+            }
+        }
+
+        return hasNegative ? product : 1;
+    }
+}
